34ec3024556e9f87dc277a89335d7dd79c97333d
Alexandr Chekaldin <alek-chekaldin@ya.ru>
2023-01-31 10:45:34 +0300

    добавил папки задач

---
+++ task25/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 25: Напишите функцию, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Нельзя использовать библиотеку Math!
+
+
+int Extent(int a, int b)
+{
+    int result=1;
+    for (int count=1; count<=b; count++)
+    {
+        result=result*a;
+    }
+    return result;
+}
+
+int ReadConsole (string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine()!);
+}
+
+Console.Clear();
+
+int a = ReadConsole ("Введите число А: ");
+
+int b = ReadConsole ("Введите число Б: ");
+
+int pow = Extent (a,b);
+
+Console.Write (pow);
---
+++ task27/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+void Dellet(string[] args)
+{
+    Console.WriteLine("Введите число: ");
+    int n=int.Parse(Console.ReadLine()!);
+    int sum=0;
+    int temp=0;
+    {
+        while (n>0)
+        {
+            temp=n%10;
+            sum=sum+temp;
+            n=n/10;
+        }
+        Console.WriteLine($"Сумма цифр числа равна {sum}");
+    }
+}
+Dellet(args);
